846f27fe820a5f9365864107b500ef76855488f6
sk-235648 <sujalgupta1242005@gmail.com>
2025-05-28 21:00:20 +0530

    dashboard

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import uploadRouter from './routes/upload.js';
 import dotenv from "dotenv";
+
 const app = express();
 dotenv.config();
+
 const port = process.env.PORT || 5000;
+
 app.use("/api/upload", uploadRouter);
+
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
--- components/dashboard.jsx
+++ components/dashboard.jsx
@@ -5,8 +5,8 @@ const dashboard = () => {
   return (
     <div className='h-screen w-full flex justify-center items-center'>
       <form action="http://localhost:5000/api/upload" method="post" encType="multipart/form-data">
-        <input type="file" name="file" />
-        <button type="submit">Upload</button>
+        <input className="border-2 border-gray-300 rounded-md p-2" type="file" name="file" />
+        <button className="bg-blue-500 text-white p-2 rounded-md" type="submit">Upload</button>
       </form>
     </div>
   )
