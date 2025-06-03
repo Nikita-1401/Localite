498e91f333940204bd562d675d77cd5cfd485890
sk-235648 <sujalgupta1242005@gmail.com>
2025-06-03 12:57:34 +0530

    login and signup

--- backend/lib/db.js
+++ backend/lib/db.js
@@ -2,13 +2,9 @@
 
 import mongoose from 'mongoose';
 
-const mongo_url = process.env.MONGO_CONN;
-
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/Localite',{
-            useNewUrlParser: true,
-        });
+        const conn = await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/Localite');
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (err) {
         console.log('MongoDB Connection Error: ', err);
