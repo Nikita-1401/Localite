adc37a15d02e093f7c8333fa7c691f7164154336
Nikita-1401 <nikitadtoasd@gmail.com>
2025-06-11 18:24:21 +0530

    db

--- backend/lib/db.js
+++ backend/lib/db.js
@@ -6,10 +6,10 @@ const connectDB = async () => {
     const conn = await mongoose.connect(process.env.MONGODB_URI);
     console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
-    console.error("✖️ MongoDB connection error:", err.message);
+    console.error(" MongoDB connection error:", err.message);
     process.exit(1);
   }
 };
 
 export default connectDB;
- 
+
